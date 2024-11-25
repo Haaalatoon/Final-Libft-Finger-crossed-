e3e6f7fe9dad0f477b14421e1ada2c090b8b5e90
Haaalatoon <rhilane.hala@gmail.com>
2024-11-25 02:14:22 +0100

    Updated ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -18,8 +18,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len2;
 	size_t	len1;
 
-	if (!s1 || !s2)
-		return (NULL);
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
 	s = (char *)malloc(len1 + len2 + 1);
